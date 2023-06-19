d682774969f00e7ff1aa2059d89ed085876c2c3f
Kiryl Kotau <kiryl.kotau@gmail.com>
2023-06-19 17:37:50 +0200

    feat: Configure SCSS modules

--- config/build/buildloaders.ts
+++ config/build/buildloaders.ts
@@ -1,11 +1,25 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      'style-loader', // Creates `style` nodes from JS strings
-      'css-loader', // Translates CSS into CommonJS
+      // 'style-loader', // Creates `style` nodes from JS strings
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // Add instead of style-loader
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resourcePath: string) =>
+              Boolean(resourcePath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]' // CSS class names
+              : '[hash:base64:8]',
+          },
+        },
+      }, // Translates CSS into CommonJS and configure css modules
       'sass-loader', // Compiles Sass to CSS
     ],
   };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Counter } from './components/Counter';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
