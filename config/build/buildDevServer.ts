f83c9dca5487359a964950c96c4cd1cb0ff0443e
Kiryl Kotau <kiryl.kotau@gmail.com>
2023-06-19 22:51:45 +0200

    feat: Add routing, lazy, suspense

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevServerConficuration {
   return {
     port: options.port,
     open: true,
+    historyApiFallback: true, // To process all requests that don't match existing files on the server and direct them to the same app page (for example, to the main page or to the router), which will handle routing on the client side
   };
 }
