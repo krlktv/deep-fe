a3fe998153b8befe5cfa4234a7253679ecdf79a8
Kiryl Kotau <kiryl.kotau@gmail.com>
2023-06-21 01:37:59 +0200

    feat: Add themes and SCSS variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
-      <Link to='/'>Main</Link>
-      <Link to='/about'>About</Link>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Current ({theme}) Theme</button>
+      <Link to='/'>| ... Main ... |</Link>
+      <Link to='/about'>| ... About... |</Link>
       <Suspense fallback={<div>... Loading ...</div>}>
         <Routes>
           <Route path='/about' element={<AboutPageAsync />} />
