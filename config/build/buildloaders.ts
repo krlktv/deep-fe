8262db801ef63164257607c5da5cba0b79b15593
Kiryl Kotau <kiryl.kotau@gmail.com>
2023-07-21 23:34:14 +0200

    feat: Add Babel and extract plugin

--- config/build/buildloaders.ts
+++ config/build/buildloaders.ts
@@ -3,6 +3,39 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
 export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => {
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
+  const babelLoader = {
+    test: /\.(js|ts|tsx)$/, // Regular for our file types
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [['i18next-extract', { locales: ['en', 'be'], keyAsDefaultValue: true }]],
+      },
+    },
+  };
+
+  // If don't use a TypeScript, then need to add a babel-loader
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -23,26 +56,5 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
     ],
   };
 
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
-    use: [
-      {
-        loader: 'file-loader',
-      },
-    ],
-  };
-
-  const svgLoader = {
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  };
-
-  // If don't use a TypeScript, then need to add a babel-loader
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
-
-  return [cssLoader, fileLoader, svgLoader, typescriptLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]; // The order of loaders is important
 };
