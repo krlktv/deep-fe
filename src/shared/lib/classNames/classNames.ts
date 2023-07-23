cfe37dac6198e84c15b1a6eb31b501ee84d6ac64
Kiryl Kotau <kiryl.kotau@gmail.com>
2023-07-23 02:04:06 +0200

    feat: Add Jest (unit tests)

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,6 @@
 type Mods = Record<string, boolean | string>;
 
-export const classNames = (mainClassName: string, mods?: Mods, additionalClassNames?: string[]): string => {
+export const classNames = (mainClassName: string, mods: Mods = {}, additionalClassNames: string[] = []): string => {
   return [
     mainClassName,
     ...additionalClassNames.filter(Boolean), // Filtering by a Boolean filter so that there are no undefainds
